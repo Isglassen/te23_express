350b02ffbf0295feed38c0168f5dd10880ad8b14
Isglassen <vilhelmgg@gmail.com>
2026-05-21 09:49:52 +0200

    Handle invalid json errors

--- src/server.ts
+++ src/server.ts
@@ -12,16 +12,20 @@ app.use(express.json());
 
 app.use('/manga', mangaRouter);
 
+app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+  if (err instanceof SyntaxError && 'status' in err && err.status === 400 && 'body' in err) {
+    console.error(err);
+    return res.status(400).send({ status: 400, message: err.message });
+  }
+  console.error(err.stack);
+  res.status(500).json({ error: "Internal server error" });
+});
+
 app.listen(PORT, (error) => {
   if (error)
     console.error(error)
   else
     console.log(`Server is running on port ${PORT}`);
-});
-
-app.use((err: any, req: express.Request, res: express.Response) => {
-  console.error(err.stack);
-  res.status(500).json({ error: "Internal server error" });
 });
 
 app.once("close", () => {
